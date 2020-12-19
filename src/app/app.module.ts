ef7d3122ecc7a770d6a253d9fa47a25979a5acf5
astridtchouteng <astrid.tchouteng@gmail.com>
2020-12-20 00:07:09 +0100

    dsplay product name and price

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { ProductService } from './services/product.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
+import localeFr from '@angular/common/locales/fr';
+
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeFr, 'fr');
 
 @NgModule({
   declarations: [
@@ -17,7 +22,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
     BrowserModule,
     HttpClientModule
   ],
-  providers: [ProductService],
+  providers: [ProductService, {provide: LOCALE_ID, useValue: 'fr'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
