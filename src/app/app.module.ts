39931de0512c444797777e142a8f248951b8f2ee
astridtchouteng <astrid.tchouteng@gmail.com>
2021-01-07 21:27:23 +0100

    new cart status component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
 
 registerLocaleData(localeFr, 'fr');
 
@@ -31,7 +32,8 @@ const routes: Routes = [
     AppComponent,
     ProductListComponent,
     ProductCategoryMenuComponent,
-    SearchComponent
+    SearchComponent,
+    CartStatusComponent
   ],
   imports: [
     BrowserModule,
