f15ef6672360041c0141957fb1bda9b19a9135aa
astridtchouteng <astrid.tchouteng@gmail.com>
2021-01-14 16:24:16 +0100

    checkout service

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Purchase } from '../common/purchase';
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +9,11 @@ import { Injectable } from '@angular/core';
 export class CheckoutService {
 
   private purchaseUrl = "http://localhost:8282/api/checkout/purchase";
-  
-  constructor() { }
+
+  constructor(private httpClient: HttpClient) { }
+
+  placeOrder(purchase: Purchase): Observable<any> {
+
+    return this.httpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
 }
